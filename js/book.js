a7fa383605fd3497d2a1038d1452b209e2d01a50
mycreature <aistjq1691@gmail.com>
2024-11-06 12:27:45 +0900

    [김민섭]수정:carousel 클릭시 값 출력

--- js/book.js
+++ js/book.js
@@ -5,6 +5,17 @@ const nextButton = document.querySelector('.carouel-nextbtn')
 let carouselIndex = 0;
 let hiddent_Count = 2;
 
+const planet_Price = document.querySelector('.planet-price')
+const planet_Imfr = [ {name : 'Mercurius', price : '100', image : 'url("../images/book/leftBtn.svg")'} 
+                     ,{name : 'Venus', price : '200'}
+                     ,{name : 'Mars', price : '300'} 
+                     ,{name : 'Jupiter', price : '400'} 
+                     ,{name : 'Pluto', price : '500'} 
+                     ,{name : 'Saturn', price : '600'}
+                     ,{name : 'Mercurius', price : '100'}
+                     ,{name : 'Mercurius', price : '100'}     
+                    ]
+                    
 prevButton.addEventListener('click', () => {
 
     if (carouselIndex === 0) return;
@@ -33,6 +44,8 @@ carousel_Button.forEach((selected_Button, index) => {
     selected_Button.addEventListener("click", () => {
         const selected_Img = selected_Button.querySelector("img");
         selected_Img.classList.add("sizeup-animation");
+        planet_Price.innerHTML = `<strong>${planet_Imfr[index].name}</strong><br>$${planet_Imfr[index].price}(price) + $${planet_Imfr[index].price}(deposit)`;
+
 
         carousel_Button.forEach((other_Button, otherIndex) => {
             const other_Img = other_Button.querySelector("img");
