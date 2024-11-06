0c10826c51af6cec8e18e726f32b09dbf992a334
mycreature <aistjq1691@gmail.com>
2024-11-06 14:20:27 +0900

    [김민섭]수정:carousel 클릭시 side 배경 변경

--- js/book.js
+++ js/book.js
@@ -5,17 +5,20 @@ const nextButton = document.querySelector('.carouel-nextbtn')
 let carouselIndex = 0;
 let hiddent_Count = 2;
 
+const side_Img = document.querySelector('.side');
+const localhostUrl = window.location.origin;
+console.log(localhostUrl)
+
 const planet_Price = document.querySelector('.planet-price')
-const planet_Imfr = [ {name : 'Mercurius', price : '100', image : 'url("../images/book/leftBtn.svg")'} 
-                     ,{name : 'Venus', price : '200'}
-                     ,{name : 'Mars', price : '300'} 
-                     ,{name : 'Jupiter', price : '400'} 
-                     ,{name : 'Pluto', price : '500'} 
-                     ,{name : 'Saturn', price : '600'}
-                     ,{name : 'Mercurius', price : '100'}
-                     ,{name : 'Mercurius', price : '100'}     
+const planet_Imfr = [ {name : 'Mercurius', price : '100', image : `/assets/images/book/planet/Mercurius.svg`} 
+                     ,{name : 'Venus',     price : '200', image : `/assets/images/book/planet/Venus.svg`} 
+                     ,{name : 'Mars',      price : '300', image : `/assets/images/book/planet/Mars.svg`} 
+                     ,{name : 'Jupiter',   price : '400', image : `/assets/images/book/planet/Jupiter.svg`} 
+                     ,{name : 'Pluto',     price : '500', image : `/assets/images/book/planet/Pluto.svg`} 
+                     ,{name : 'Saturn',    price : '600', image : `/assets/images/book/planet/Saturn.svg`} 
+                     ,{name : 'Uranus',    price : '700', image : `/assets/images/book/planet/Uranus.svg`} 
+                     ,{name : 'Neptune',   price : '800', image : `/assets/images/book/planet/Neptune.svg`}    
                     ]
-                    
 prevButton.addEventListener('click', () => {
 
     if (carouselIndex === 0) return;
@@ -46,6 +49,7 @@ carousel_Button.forEach((selected_Button, index) => {
         selected_Img.classList.add("sizeup-animation");
         planet_Price.innerHTML = `<strong>${planet_Imfr[index].name}</strong><br>$${planet_Imfr[index].price}(price) + $${planet_Imfr[index].price}(deposit)`;
 
+        side_Img.style.backgroundImage = `url("${localhostUrl}${planet_Imfr[index].image}")`
 
         carousel_Button.forEach((other_Button, otherIndex) => {
             const other_Img = other_Button.querySelector("img");
