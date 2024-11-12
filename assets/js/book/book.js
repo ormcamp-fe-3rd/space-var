9bdbdf69a82e49baf57291be6afc9315920c52ea
MH <dico659@gmail.com>
2024-11-12 22:02:22 +0900

    [신명희]수정: email 검증 버그 수정

--- assets/js/book/book.js
+++ assets/js/book/book.js
@@ -190,10 +190,11 @@ function checkPhoneRegex(event) {
 // ...@...
 function checkEmailRegex(event) {
   event.target.value = event.target.value
-    .replace(/[^a-zA-Z0-9@.]/g, "") // 영문, 숫자, @, . 만 허용
+    .replace(/[^a-zA-Z0-9@._-]/g, "")
     .trim();
 
-  return event.target.value.includes("@");
+  const emailRegex = /^.+@.+$/;
+  return emailRegex.test(event.target.value);
 }
 
 // CARD NUMBER
@@ -235,25 +236,23 @@ function checkInputValidation(event) {
   switch (id) {
     case "name":
       formValidation.setNameValid(checkNameRegex(event).length > 0); // 한 글자라도 적히면 됨
-      resetCertification();
       updateInputBorder(event, formValidation.isNameValid);
       break;
 
     case "birth":
       formValidation.setBirthValid(checkBirthRegex(event).length === 10); // 슬래시 포함 10자
-      resetCertification();
       updateInputBorder(event, formValidation.isBirthValid);
       break;
 
     case "phone":
       formValidation.setPhoneValid(checkPhoneRegex(event).length === 13); // 하이픈 포함 13자
-      resetCertification();
+      resetCertification(event);
       updateInputBorder(event, formValidation.isPhoneValid);
       break;
 
     case "email":
       formValidation.setEmailValid(checkEmailRegex(event)); // 이메일은 길이 제한이 없음. (양식 맞는지 판별만 하고 boolean 값 반환)
-      resetCertification();
+      resetCertification(event);
       updateInputBorder(event, formValidation.isEmailValid);
       break;
 
@@ -261,7 +260,6 @@ function checkInputValidation(event) {
       formValidation.setCardNumberValid(
         checkCardNumberRegex(event).length === 19
       ); // 하이픈 포함 19자
-      resetCertification();
       updateInputBorder(event, formValidation.isCardNumberValid);
       break;
 
@@ -269,13 +267,11 @@ function checkInputValidation(event) {
       formValidation.setExpirationValid(
         checkExpirationRegex(event).length === 5
       ); // 슬래시 포함 5자
-      resetCertification();
       updateInputBorder(event, formValidation.isExpirationValid);
       break;
 
     case "security-code":
       formValidation.setSecurityValid(checkSecurityRegex(event).length === 4); // 4글자
-      resetCertification();
       updateInputBorder(event, formValidation.isSecurityValid);
       break;
   }
@@ -297,18 +293,18 @@ function updateInputBorder(event, valid) {
 }
 
 // 전화, 이메일 입력란 수정할 시, CERTIFICATION도 reset
-function resetCertification() {
-  const certificationBtns = bookForm.querySelectorAll(".certification-btn");
+function resetCertification(event) {
+  const confirmBtns = bookForm.querySelectorAll(".confirm-btn");
 
-  if (!formValidation.isPhoneValid) {
-    certificationBtns[0].className = "certification-btn not-verified";
-    certificationBtns[0].textContent = "CERTIFICATION";
+  if (event.target.id === "phone") {
+    confirmBtns[0].className = "confirm-btn not-verified";
+    confirmBtns[0].textContent = "CONFIRM";
     return;
   }
 
-  if (!formValidation.isEmailValid) {
-    certificationBtns[1].className = "certification-btn not-verified";
-    certificationBtns[1].textContent = "CERTIFICATION";
+  if (event.target.id === "email") {
+    confirmBtns[1].className = "confirm-btn not-verified";
+    confirmBtns[1].textContent = "CONFIRM";
     return;
   }
 }
@@ -323,17 +319,17 @@ function checkCertication(event) {
   // 실제 API 구현은 현재 힘드므로, 전화번호와, 이메일 양식이 맞으면 검증되도록 함
   // 전화번호 검증 버튼/이메일 검증 중 선택된 버튼에 따라 isValid에 값이 다르게 담김(관련 입력란 검증 값 가져옴)
   const isValid =
-    id === "certification-phone"
+    id === "confirm-phone"
       ? formValidation.isPhoneValid
       : formValidation.isEmailValid;
 
   // 전화번호 버튼일 때 isValid의 값이 바뀌고 그 값이 들어감. 아니라면 이전 값 유지
   formValidation.setPhoneVerified(
-    id === "certification-phone" ? isValid : formValidation.isPhoneVerified
+    id === "confirm-phone" ? isValid : formValidation.isPhoneVerified
   );
 
   formValidation.setEmailVerified(
-    id === "certification-email" ? isValid : formValidation.isEmailVerfied
+    id === "confirm-email" ? isValid : formValidation.isEmailVerfied
   );
 
   // 실제 동작처럼 보이도록 setTimeout 사용
@@ -342,10 +338,10 @@ function checkCertication(event) {
   // isValid 여부에 따라 VERIFIED, FAILED로 버튼 스타일 바뀜
   timer = setTimeout(() => {
     if (isValid) {
-      btn.className = "certification-btn verified";
+      btn.className = "confirm-btn verified";
       btn.textContent = "VERIFIED";
     } else {
-      btn.className = "certification-btn rejected";
+      btn.className = "confirm-btn rejected";
       btn.textContent = "FAILED";
     }
   }, 1000);
@@ -399,14 +395,14 @@ function handleCheckboxClick(event) {
 const bookForm = document.querySelector(".book-form");
 
 const inputs = bookForm.querySelectorAll(".input");
-const certificationBtns = bookForm.querySelectorAll(".certification-btn");
+const confirmBtns = bookForm.querySelectorAll(".confirm-btn");
 const checkboxes = bookForm.querySelectorAll(".checkbox-hidden");
 
 inputs.forEach((input) => {
   input.addEventListener("input", handleFormInput);
 });
 
-certificationBtns.forEach((certificationBtn) => {
+confirmBtns.forEach((certificationBtn) => {
   certificationBtn.addEventListener("click", handleCertificationBtnCilick);
 });
 
