32f1333b9f10ee6c67886ec4d1faf1a40bf5d134
MH <dico659@gmail.com>
2024-11-19 20:55:47 +0900

    [전체]수정: start 스크롤바 버그
    Co-authored-by: ksj0621<ksj0621@users.noreply.github.com>
    Co-authored-by: Park junghye <YellowFiber@users.noreply.github.com>
    Co-authored-by: 윤환 <yoonwhan-ji@users.noreply.github.com>
    Co-authored-by: mycreature <mycreature@users.noreply.github.com>

--- src/start/const/index.js
+++ src/start/const/index.js
@@ -1,1 +1,2 @@
 export const FINAL_SECTION_INDEX = 2;
+export const TOTAL_SECTION_COUNT = 3;
--- src/start/start.js
+++ src/start/start.js
@@ -1,4 +1,4 @@
-import { FINAL_SECTION_INDEX } from "./const/index.js";
+import { FINAL_SECTION_INDEX, TOTAL_SECTION_COUNT } from "./const/index.js";
 
 function createSectionState() {
   let index = 0;
@@ -32,7 +32,6 @@ function createSectionState() {
 let currentSectionState = createSectionState();
 
 function checkScrollSection(deltaY) {
-
   let currentSectionIndex = currentSectionState.getCurrentIndex();
 
   if (deltaY > 50 && currentSectionIndex < FINAL_SECTION_INDEX) {
@@ -51,35 +50,29 @@ function checkScrollSection(deltaY) {
 function updateScrollFillStyle() {
   let currentSectionIndex = currentSectionState.getCurrentIndex();
   const scrollFill = document.querySelector(".scroll-fill");
-  const videos = document.querySelectorAll(".video");
 
-  const totalSections = videos.length - 1;
-  const progress = (currentSectionIndex / totalSections) * 100;
+  const progress = (currentSectionIndex / TOTAL_SECTION_COUNT) * 100;
+  console.log(progress);
   scrollFill.style.height = 100 - progress + "%";
 }
 
 function switchActiveVideoContents() {
-
   let currentSectionIndex = currentSectionState.getCurrentIndex();
-  
+
   const text = document.querySelector(".video-text");
   const videoSection = document.querySelector(".video-section");
   const video = videoSection.querySelector("video");
 
   if (currentSectionIndex === 1) {
     video.src = "/src/start/assets/videos/spaceship.mp4";
-    text.textContent = "This is your life chance to change everything."
-    
+    text.textContent = "This is your life chance to change everything.";
   } else if (currentSectionIndex === 2) {
     video.src = "/src/start/assets/videos/night.mp4";
-    text.textContent = "Take your opportunity, We will join you."
+    text.textContent = "Take your opportunity, We will join you.";
   } else {
     video.src = "/src/start/assets/videos/earth.mp4";
     text.classList.toggle("active");
-    text.textContent = "Have you ever thought about leaving the Earth?"
-
-
-
+    text.textContent = "Have you ever thought about leaving the Earth?";
   }
 }
 
